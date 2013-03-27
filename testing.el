92dfa5d0032bcbbc53bcba64562cfdc3a4342ee4
gnrr <hideaki.sh@gmail.com>
2013-03-27 09:59:58 +0900

    M-RET: browse-url-at-point

--- testing.el
+++ testing.el
@@ -4,6 +4,19 @@
 ;;;
 
 ;;
+;; 
+;;
+(defun browse-url-at-point ()
+  (interactive)
+  (let ((url-region (bounds-of-thing-at-point 'url)))
+    (when url-region
+      (browse-url (buffer-substring-no-properties (car url-region)
+						  (cdr url-region))))))
+
+(global-set-key "\M-\C-m" 'browse-url-at-point)      ; M-RET
+
+
+;;
 ;; powerline - https://github.com/milkypostman/powerline
 ;;
 (require 'powerline)
