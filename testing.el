efb83ca76a615a4fe40fac9c7480ede52415abf7
gnrr <hideaki.sh@gmail.com>
2013-10-03 10:28:17 +0900

    add: popwin and slime

--- testing.el
+++ testing.el
@@ -4,6 +4,26 @@
 ;;;
 
 ;;
+;; slime
+;;
+(add-to-list 'load-path "/opt/local/share/emacs/site-lisp/slime")
+(setq slime-lisp-implementations
+     `((sbcl ("/opt/local/bin/sbcl"))
+       (abcl ("/opt/local/bin/abcl"))
+       (clisp ("/opt/local/bin/clisp"))))
+
+(setq slime-default-lisp 'clisp)
+(require 'slime)
+;; (slime-setup  '(slime-repl slime-asdf slime-fancy slime-banner))
+(slime-setup  '(slime-repl slime-fancy slime-banner))
+
+;;
+;; popwin
+;;
+(require 'popwin)
+(setq display-buffer-function 'popwin:display-buffer)
+
+;;
 ;; my-kill-buffer
 ;;
 (defun my-kill-buffer (arg)
